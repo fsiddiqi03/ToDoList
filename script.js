94c824017d3c0a3030e771c843b6c72d0db7b806
Faris Siddiqi <farisiddiqi@gmail.com>
2023-12-23 22:52:03 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const InputBox = document.getElementById('input-box');
+const ListContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(InputBox.value === ''){
+        alert('No task typed')
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = InputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    InputBox.value = '';
+    saveData();
+}
+
+
+ListContainer.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+
+}, false);
+
+
+function saveData(){
+    localStorage.setItem('data', ListContainer.innerHTML);
+}
+
+function showTask(){
+    ListContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
